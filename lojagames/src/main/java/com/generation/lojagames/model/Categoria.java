29ce488aed9e094530fa2c507700fb9a8c0c4dc0
Alessandro Castro <97323030+alecfonseca@users.noreply.github.com>
2022-02-23 18:37:57 -0300

    lojaGames

---
+++ lojagames/src/main/java/com/generation/lojagames/model/Categoria.java
@@ -0,0 +1,56 @@
+package com.generation.lojagames.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "Genêro é obrigatório!")
+	private String genero;
+	
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return genero;
+	}
+
+	public void setTipo(String tipo) {
+		this.genero = tipo;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+
+	
+}
